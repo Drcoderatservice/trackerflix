43c894d3dc85fc48b121c143bd27e43b97be4786
Aditya Anand <124674114+Drcoderatservice@users.noreply.github.com>
2026-04-17 10:50:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let selectedAnime=null;
 async function searchAnime(){
 
 let query=document.getElementById("animeSearchInput").value;
+  let category = selectedCategory;
 let category = document.getElementById("categorySelect")?.value;
 
 if(category==="Movies" || 
